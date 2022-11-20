c2f40f63bbf9d5931267fa9a25a78216c36faa3b
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-20 22:37:57 +0300

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Николай Никитин on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
